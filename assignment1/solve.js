ca71957457b4ae7e24d6fd267095a6c3afe01931
Truonghdpk <truongondaihoc@gmail.com>
2020-09-13 16:55:32 +0700

    improve code

--- assignment1/solve.js
+++ assignment1/solve.js
@@ -1,17 +1,18 @@
 let button = document.getElementById("guessButton");
 let countWrong = 0;
+let random = Math.round(Math.random() * 10) % 10;
 button.addEventListener("click", function () {
   // What happens when you click the button?
   // Hint: Perhaps what happens next in Milestone 4.
   if (countWrong >= 10) return;
   let inputValue = document.getElementById("number").value;
   console.log(inputValue);
-  let random = Math.round(Math.random() * 10) % 5;
   if (random === +inputValue) {
     document.getElementById("message").style.backgroundColor = "green";
     document.getElementById("message").innerHTML =
       "Yah YOU ARE THE LORD OF THE GUESSES";
       countWrong = 0;
+      random = Math.round(Math.random() * 10) % 10;
   } else {
     countWrong++;
     document.getElementById("message").style.backgroundColor = "red";
